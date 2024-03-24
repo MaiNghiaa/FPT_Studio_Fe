475f2f59e09112d37eec3dd6167793730e1e0f89
Trung Nghĩa <109063567+MaiNghiaa@users.noreply.github.com>
2024-03-25 01:20:15 +0700

    Update folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+
 Sentry.init({
   dsn: 'YOUR_SENTRY_DSN',
   integrations: [
