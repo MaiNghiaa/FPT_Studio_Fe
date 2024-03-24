4280338b2e22b5b52b6bc17abf4efb0beb4e7c03
Trung Nghĩa <109063567+MaiNghiaa@users.noreply.github.com>
2024-03-24 17:49:55 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import reportWebVitals from './reportWebVitals';
+Sentry.init({
+  dsn: 'YOUR_SENTRY_DSN',
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+      <App />
+    </Sentry.ErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
