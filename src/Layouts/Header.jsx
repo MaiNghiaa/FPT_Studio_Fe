3b00ac05d8117637dd35d4ce89d4068866f2632f
Trung Nghĩa <109063567+MaiNghiaa@users.noreply.github.com>
2024-03-25 17:43:57 +0700

    update Header_Footer

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 // import Image from "../../assets/Store/637961834239224907_fs1.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@awesome.me/kit-KIT_CODE/icons/classic/solid'
 
 export default function Header() {
   return (
     <div className="Header">
-      {/* width: 100%; max-width: 1200px; padding-right: 12px; padding-left: 12px;
-      margin-right: auto; margin-left: auto; */}
-      <div className="Header_top bg-black">
-        <div className=" w-full w-[100%] max-w-[1200px] px-[12px] mx-auto">
-          <div className="wrapper py-[8px] flex items-center justify-center">
+    
+      <div className="Header_top bg-black py-5">
+        <div className="flex items-center justify-center w-full max-w-[1200px] px-[12px] mx-auto ">
             <div className="header-logo max-w-[276px] flex-auto">
               <a href="\">
                 <img
@@ -18,27 +18,29 @@ export default function Header() {
                 />
               </a>
             </div>
-            <div className="header-search flex-1 bg-[rgba(255,255,255,.15)]">
+            <div className="header-search flex-1 bg-[rgba(255,255,255,.15)] mx-[125.953px]">
               <form action>
-                <div className="flex  border-none px-[16px] min-h-[28px] text-[#939ca3] w-[476px] mx-0 my-auto  ">
+                <div className="flex  border-none px-[16px] min-h-[28px] text-[#939ca3] w-[476px]">
                   <span>
-                    <i className="el el-search"></i>
-                  </span>
+                  <FontAwesomeIcon icon={fak.faMyIcon} />
+                                    </span>
                   <input
                     type="text"
                     name=""
                     id=""
-                    className="border-none outline-none bg-transparent"
+                    className="border-none outline-none bg-transparent text-[14px] leading-5 flex-1 mr-[8px] py-[1px] px-[2px]"
+                    placeholder="Bạn đang tìm sản phẩm, tin tức, workshop..."
                   />
                 </div>
               </form>
             </div>
-            <div className="header-cart flex-1"></div>
+            <div className="header-cart flex-1 text-white">asdasdasd</div>
             <p></p>
-          </div>
         </div>
-        <div className="header-bot bg-[#212529] flex justify-center items-center">
-          <div className="wrapper w-[100%] max-w-[1200px] px-[12px] mx-[auto]">
+        
+      </div>
+      <div className="header-bot bg-[#212529] flex justify-center items-center">
+          <div className="wrapper w-[100%] max-w-[1200px] px-[12px] mx-[auto] flex items-center">
             <ul className="flex">
               <li className="block py-[19px] px-0 text-[#939ca3] text-[14px] leading-[14px] font-[400] min-w-[122px]">
                 <a href="\">Iphone</a>
@@ -61,19 +63,21 @@ export default function Header() {
               <li className="block py-[19px] px-0 text-[#939ca3] text-[14px] leading-[14px] font-[400] min-w-[122px]">
                 <a href="\">Workshop</a>
               </li>
-              <li className="block py-[19px] px-0 text-[#939ca3] text-[14px] leading-[14px] font-[400] min-w-[122px]">
+              <li className="block py-[19px] px-0 text-[#939ca3] text-[14px] leading-[14px] font-[400] min-w-[122px] text-[#f6c743] font-medium">
                 <a href="\">Khuyến mãi</a>
               </li>
               <li className="block py-[19px] px-0 text-[#939ca3] text-[14px] leading-[14px] font-[400] min-w-[122px]">
                 <a href="\">F.Care</a>
               </li>
             </ul>
-            <div className="phone-hotline">
-              <a href="tel:+84123456789">Gọi 1800 6601</a>
-            </div>
+        
+            <button className="phone-hotline ml-[52px] border-1">
+              <a href="tel:+84123456789" className="text-[#fff] bg-[#0664f9] outline-none px-[12px] h-[28px] flex items-center justify-center"><i className="w-[16px] text-[16px]">*</i>
+              <span className="whitespace-nowrap">Gọi 1800 6601</span>
+              </a>
+            </button>
           </div>
         </div>
-      </div>
     </div>
   );
 }
--- src/Utils/index.jsx
+++ src/Utils/index.jsx
@@ -1,3 +1,8 @@
-import logo from "../../public/assets/logo/logo-fstu-aar.webp";
 
+import logo from "/assets/logo/logo-fstu-aar.webp";
+import Gallery1 from "/assets/images/Store/Gallery1.jpg";
 export const image_logo = logo;
+
+
+
+export const Gallery_1 = Gallery1;
